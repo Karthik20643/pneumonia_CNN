c9236eed04b865803e1bb0a4b2a38a944a2126a5
karthik s s <karthikss8590@gmail.com>
2026-03-25 13:46:48 +0530

    training finished for pneumonic cnn model

--- model.py
+++ model.py
@@ -2,20 +2,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class PneumoniaCNN(nn.Module):
 
-class pneumoniaCNN(nn.Module):
-
-def __init__ (self) : 
-
-
+    def __init__(self):
         super(PneumoniaCNN, self).__init__()
 
-
-    self.conv1 = nn.Conv2d(3, 32, 3)
+        self.conv1 = nn.Conv2d(3, 32, 3)
         self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
 
-                self.conv2 = nn.Conv2d(32, 64, 3)
-
- self.fc1 = nn.Linear(64 * 54 * 54, 128)
+        self.fc1 = nn.Linear(64 * 54 * 54, 128)
         self.fc2 = nn.Linear(128, 2)
-        
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        x = x.view(-1, 64 * 54 * 54)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
--- train.py
+++ train.py
@@ -3,6 +3,10 @@ import torchvision
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from model import PneumoniaCNN
+import torch.optim as optim
+import torch.nn as nn
+
 
 transform = transforms.Compose([
 
@@ -20,8 +24,43 @@ train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
+model = PneumoniaCNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+print("model loaded successfully")
+
 
 print("Number of training images:", len(train_dataset))
 print("Number of validation images:", len(val_dataset))
 print("Number of test images:", len(test_dataset))
+
+
+epochs = 5 
+
+for epoch in range(epochs) :
+
+    total = 0 
+    correct = 0
+    running_loss = 0.0
+
+
+for images , labels in train_loader : 
+
+    optimizer.zero_grad() 
+    outputs = model(images)
+    loss = criterion(outputs, labels)
+    loss.backward()            
+    optimizer.step()    
+
+running_loss += loss.item()
+_, predicted = torch.max(outputs, 1)
+total += labels.size(0)
+correct += (predicted == labels).sum().item()
+
+
+accuracy = 100 * correct / total
+print(f"Epoch [{epoch+1}/{epochs}], Loss: {running_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+
+print("training completed")
 
